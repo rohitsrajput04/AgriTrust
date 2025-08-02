8c6d6fdb5f8e8665ead927c1901a1c11910adce0
RohitRajput04 <55823093+rohitsrajput04@users.noreply.github.com>
2025-08-02 09:52:48 +0530

    Update AppTest.java

--- AgriGatewayService/src/test/java/com/agritrust/gateway/AppTest.java
+++ AgriGatewayService/src/test/java/com/agritrust/gateway/AppTest.java
@@ -3,6 +3,7 @@ package com.agritrust.gateway;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 public class AppTest {
     @Test
     void testApp() {
